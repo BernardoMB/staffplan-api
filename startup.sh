1f888f7caedfda24e8e0c1737f0f7b834128b1ae
minh dong <minh@moblabs.com>
2019-01-26 21:44:26 -0800

    Update startup script

--- startup.sh
+++ startup.sh
@@ -2,10 +2,9 @@ npm install
 cd public
 npm install
 cd ..
-forever stopall
-forever start app.js
+pm2 kill
+pm2 start app.js
 cd public
 ng build --prod
 cd dist/staff-planner
-pm2 kill
 pm2 start /usr/local/bin/http-server --name staff-plan -- -p 8080 -d false
