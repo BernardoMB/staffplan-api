756a9a77b24dd9bef7b32e8c26578834e3a4f5be
Muthiah, Ramachandran <ramachandran.muthiah@pearson.com>
2019-03-19 16:31:25 +0530

    Updated query and logic for staff gap

--- svc/modules/dashboard/dashboard.js
+++ svc/modules/dashboard/dashboard.js
@@ -9,6 +9,32 @@ const getValue = (rows => {
   return 0;
 })
 
+const calculateGap = (StaffAssignments => {
+  let gap = 0;
+  if (StaffAssignments && StaffAssignments.length) {
+    StaffAssignments.forEach((currentInfo, index) => {
+      // if staff doesn't have project start date add to gap
+      if (currentInfo.START_DATE) {
+        // Check itteration reached end
+        if (index < StaffAssignments.length - 1) {
+          const nextInfo = StaffAssignments[index + 1];
+          // check next staff equals to current one
+          if (nextInfo.STAFF_ID === currentInfo.STAFF_ID) {
+            // check the current project end date with next project start date
+            if (nextInfo.START_DATE > currentInfo.END_DATE) {
+              // console.log(nextInfo.STAFF_ID);
+              gap++;
+            }
+          }
+        }
+      } else {
+        gap++
+      }
+    })
+  }
+  return gap;
+});
+
 const getDashboardDetails = async (req, res) => {
   const connection = await db.masterDB(req);
   const officeId = req.params.officeId;
@@ -28,7 +54,8 @@ const getDashboardDetails = async (req, res) => {
   const AdminStaff = await db.execute(connection, SQL.AdminStaff());
   const OPSStaff = await db.execute(connection, SQL.OPSStaff());
   const OnBench = await db.execute(connection, SQL.OnBench());
-  const StaffingGap = await db.execute(connection, SQL.StaffingGap());
+  const StaffAssignments = await db.execute(connection, SQL.StaffingGap());
+  const StaffingGap = calculateGap(StaffAssignments);
   const OverUnderAllocation = await db.execute(connection, SQL.OverUnderAllocation());
   const AssignmentEnding = await db.execute(connection, SQL.AssignmentEnding());
 
@@ -43,7 +70,7 @@ const getDashboardDetails = async (req, res) => {
     AdminStaff: getValue(AdminStaff),
     OPSStaff: getValue(OPSStaff),
     OnBench: getValue(OnBench),
-    StaffingGap: getValue(StaffingGap),
+    StaffingGap,
     OverUnderAllocation: getValue(OverUnderAllocation),
     AssignmentEnding: getValue(AssignmentEnding)
   });
