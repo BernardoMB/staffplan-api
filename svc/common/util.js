a1b1cca58786d923043c040dd17bf99c3f35ed39
Muthiah, Ramachandran <>
2019-06-11 08:48:13 +0530

    Enabling forgot password flow

--- svc/common/util.js
+++ svc/common/util.js
@@ -2,7 +2,12 @@ module.exports = {
   errorResponse: (res, message = "Something went wrong", status = 500) => {
     res.status(status).json({ error: true, message });
   }, 
-  successResponse: (res, data) => {
+  successResponse: (res, data = {}) => {
     res.status(200).json(data);
+  },
+  failureResponse: (res, errorCode = 'EC_UNKNOWN') => {
+    res.status(200).json({
+      errorCode
+    });
   }
-}
+};
