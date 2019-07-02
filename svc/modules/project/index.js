46ab53f60aa5e7415f16b8749dc5eff2807131b7
Anna Rathna T <annarathna.t@gmail.com>
2019-07-02 19:58:44 +0530

    Updated service for OpenRoles and PojectTeam

--- svc/modules/project/index.js
+++ svc/modules/project/index.js
@@ -5,4 +5,6 @@ const MODULE = 'project';
 module.exports = (app) => {
   app.post(`/${CONST.API}/${MODULE}/list`, authenticate.isAuthenticated, project.getProjectList);
   app.get(`/${CONST.API}/${MODULE}/:id/team`, authenticate.isAuthenticated, project.getTeam);
+  app.post(`/${CONST.API}/${MODULE}/openroles`, authenticate.isAuthenticated, project.getOpenRoles);
+  app.post(`/${CONST.API}/${MODULE}/teams`, authenticate.isAuthenticated, project.getProjectTeams);
 }
--- svc/modules/project/project.js
+++ svc/modules/project/project.js
@@ -14,7 +14,21 @@ const getTeam = async (req, res) => {
   util.successResponse(res, teamList);
 }
 
+const getOpenRoles = async (req, res) => {
+  const connection = await db.connection(req);
+  const openRoles = await db.execute(connection, SQL.getOpenRoles());
+  util.successResponse(res, openRoles);
+}
+
+const getProjectTeams = async (req, res) => {
+  const connection = await db.connection(req);
+  const projectTeams = await db.execute(connection, SQL.getProjectTeams());
+  util.successResponse(res, projectTeams);
+}
+
 module.exports = {
   getProjectList,
-  getTeam
+  getTeam,
+  getOpenRoles,
+  getProjectTeams
 }
