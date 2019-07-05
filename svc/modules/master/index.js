eae68e12b93b07f57f3aa9203f63678804043f06
Muthiah, Ramachandran <>
2019-07-05 17:04:57 +0530

    Update master service

--- svc/modules/master/index.js
+++ svc/modules/master/index.js
@@ -3,7 +3,7 @@ const authenticate = require('../auth/authenticate');
 const CONST = require('../../common/const');
 const MODULE = 'common';
 module.exports = (app) => {
-  app.get(`/${CONST.API}/commonListing/:modelName`, authenticate.isAuthenticated, master.getMasterList);
+  app.get(`/${CONST.API}/master/:modelName`, authenticate.isAuthenticated, master.getMasterList);
   app.get(`/${CONST.API}/getOfficeNameListing`, authenticate.isAuthenticated, master.getOfficeList);
   app.get(`/${CONST.API}/${MODULE}/label`, authenticate.isAuthenticated, master.getCustomLabel);
   app.post(`/${CONST.API}/${MODULE}/updatePreference`, authenticate.isAuthenticated, master.updatePreference);
