3a1ecaaf6189ec8c76dbebad8e06f975bbc95775
Anna Rathna T <annarathna.t@gmail.com>
2019-07-09 18:01:18 +0530

    Updated project service to support CUR for project details, notes, open roles

--- svc/modules/project/index.js
+++ svc/modules/project/index.js
@@ -5,8 +5,16 @@ const MODULE = 'project';
 module.exports = (app) => {
   app.post(`/${CONST.API}/${MODULE}/list`, authenticate.isAuthenticated, project.getProjectList);
   app.get(`/${CONST.API}/${MODULE}/:id/team`, authenticate.isAuthenticated, project.getTeam);
+  app.post(`/${CONST.API}/${MODULE}/:id/openroles`, authenticate.isAuthenticated, project.getOpenRoles);
   app.post(`/${CONST.API}/${MODULE}/openroles`, authenticate.isAuthenticated, project.getOpenRoles);
   app.post(`/${CONST.API}/${MODULE}/teams`, authenticate.isAuthenticated, project.getProjectTeams);
   app.get(`/${CONST.API}/${MODULE}/:id/details`, authenticate.isAuthenticated, project.getProjectDetail);
+  app.put(`/${CONST.API}/${MODULE}/details`, authenticate.isAuthenticated, project.insertProjectDetail);
+  app.post(`/${CONST.API}/${MODULE}/:id/details`, authenticate.isAuthenticated, project.updateProjectDetail);
   app.get(`/${CONST.API}/${MODULE}/:id/notes`, authenticate.isAuthenticated, project.getProjectNotes);
+  app.put(`/${CONST.API}/${MODULE}/:id/notes`, authenticate.isAuthenticated, project.insertProjectNotes);
+  app.post(`/${CONST.API}/${MODULE}/:id/notes`, authenticate.isAuthenticated, project.updateProjectNotes);
+  app.put(`/${CONST.API}/${MODULE}/:id/role`, authenticate.isAuthenticated, project.insertProjectRole);
+  app.post(`/${CONST.API}/${MODULE}/:id/bulkroleupdate`, authenticate.isAuthenticated, project.bulkRoleUpdate);
+
 }
