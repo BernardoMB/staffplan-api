d95344cc785881f68d19061061abfe9ad3d306a1
Muthiah, Ramachandran <>
2019-07-15 20:25:45 +0530

    Removed all pre release 1 files

--- migrations/1543229601370_INSERT_SUBSCRIPTION.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "up": "INSERT INTO SUBSCRIPTION (ID,NAME,COST) VALUES (1,'Premium',0)",
-    "down": ""
-}
--- public/src/app/handle-popover-close.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HandlePopoverCloseDirective } from './handle-popover-close.directive';
-
-describe('HandlePopoverCloseDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HandlePopoverCloseDirective();
-    expect(directive).toBeTruthy();
-  });
-});
