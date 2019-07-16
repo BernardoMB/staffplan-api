4a465a3cf4877975a173f8da3683b40e76f2f51d
Anna Rathna T <annarathna.t@gmail.com>
2019-07-16 20:18:57 +0530

    Added Staff and Customer Service

--- svc/modules/Staff/index.js
+++ svc/modules/Staff/index.js
@@ -7,5 +7,15 @@ module.exports = (app) => {
   app.post(`/${CONST.API}/${MODULE}/stafflist`, authenticate.isAuthenticated, staff.staffList);
   app.get(`/${CONST.API}/${MODULE}/:id/projectlist`, authenticate.isAuthenticated, staff.getStaffProjectList); 
   app.get(`/${CONST.API}/${MODULE}/allocation`, authenticate.isAuthenticated, staff.getMonthwiseAllocation);
-
+  app.put(`/${CONST.API}/${MODULE}/staffinfo`, authenticate.isAuthenticated, staff.insertStaff);
+  app.post(`/${CONST.API}/${MODULE}/:id/staffinfo`, authenticate.isAuthenticated, staff.updateStaff);
+  app.put(`/${CONST.API}/${MODULE}/:id/staffcertification`, authenticate.isAuthenticated, staff.insertStaffCertification);
+  app.delete(`/${CONST.API}/${MODULE}/:id/deletestaffcertification`, authenticate.isAuthenticated, staff.deleteStaffCertification);
+  app.put(`/${CONST.API}/${MODULE}/:id/staffexperience`, authenticate.isAuthenticated, staff.insertStaffExperience);
+  app.delete(`/${CONST.API}/${MODULE}/:id/deletestaffexperience`, authenticate.isAuthenticated, staff.deleteStaffExperience);
+  app.post(`/${CONST.API}/${MODULE}/customerlist`, authenticate.isAuthenticated, staff.customerList);
+  app.put(`/${CONST.API}/${MODULE}/customer`, authenticate.isAuthenticated, staff.insertCustomer);
+  app.post(`/${CONST.API}/${MODULE}/:id/customer`, authenticate.isAuthenticated, staff.updateCustomer);
+  app.put(`/${CONST.API}/${MODULE}/:id/customerproject`, authenticate.isAuthenticated, staff.addCustomerProject);
+  app.delete(`/${CONST.API}/${MODULE}/:id/removecustomerproject`, authenticate.isAuthenticated, staff.removeCustomerProject);
 }
--- svc/modules/Staff/query.js
+++ svc/modules/Staff/query.js
@@ -1,6 +1,7 @@
 module.exports = {
   staffAssignments: (Condition) => (
-    `SELECT 
+    `
+    SELECT 
         STAFF.FIRST_NAME,
         STAFF.MIDDLE_INITIAL,
         STAFF.LAST_NAME,
@@ -93,7 +94,7 @@ module.exports = {
         ON PROJECT.PROJECT_ID=PROJECT_STAFF.PROJECT_ID
       WHERE
         PROJECT_STAFF.STAFF_ID = ${id}
-  `
+    `
   ),
   
   getMonthwiseAllocation: () => (
@@ -111,5 +112,225 @@ module.exports = {
     FROM project_staff
     GROUP BY staff_id;
     `   
+  ),
+
+  insertStaff: (staff) => (
+    `
+    INSERT INTO STAFF (
+      FIRST_NAME,
+      MIDDLE_INITIAL,
+      LAST_NAME,
+      PREFERRED_NAME,
+      EMAIL_ID,
+      PHONE_1,
+      PHONE_1_TYPE,
+      PHONE_2,
+      PHONE_2_TYPE,
+      HOME_CITY,
+      HOME_STATE,
+      HOME_ZIP,
+      STAFF_CERTIFICATION,
+      STAFF_TRAINING,
+      STAFF_ROLE_ID,
+      STAFF_GROUP_ID,
+      STAFF_STATUS_ID,
+      OFFICE_ID,
+      EMPLOYMENT_START_DATE
+    ) VALUES (
+      '${staff.FIRST_NAME}',
+      '${staff.MIDDLE_INITIAL}',
+      '${staff.LAST_NAME}',
+      '${staff.PREFERRED_NAME}',
+      '${staff.EMAIL_ID}',
+      '${staff.PHONE_1}',
+      '${staff.PHONE_1_TYPE}',
+      '${staff.PHONE_2}',
+      '${staff.PHONE_2_TYPE}',
+      '${staff.HOME_CITY}',
+      '${staff.HOME_STATE}',
+      '${staff.HOME_ZIP}',
+      '${staff.STAFF_CERTIFICATION}',
+      '${staff.STAFF_TRAINING}',
+      ${staff.STAFF_ROLE_ID},
+      ${staff.STAFF_GROUP_ID},
+      ${staff.STAFF_STATUS_ID},
+      ${staff.OFFICE_ID},
+      '${staff.EMPLOYMENT_START_DATE}'
+    )  
+    `
+  ),
+
+
+  getStaffInfoByID: (id) => (
+    `
+    SELECT
+      *
+    FROM 
+      STAFF 
+    WHERE
+      STAFF.STAFF_ID = ${id}
+    `
+  ),
+
+  updateStaff: (id, staff) => (
+    `
+    UPDATE STAFF SET 
+      FIRST_NAME = '${staff.FIRST_NAME}',
+      MIDDLE_INITIAL = '${staff.MIDDLE_INITIAL}',
+      LAST_NAME = '${staff.LAST_NAME}',
+      PREFERRED_NAME = '${staff.PREFERRED_NAME}',
+      EMAIL_ID = '${staff.EMAIL_ID}',
+      PHONE_1 = '${staff.PHONE_1}',
+      PHONE_1_TYPE = '${staff.PHONE_1_TYPE}',
+      PHONE_2 = '${staff.PHONE_2}',
+      PHONE_2_TYPE = '${staff.PHONE_2_TYPE}',
+      HOME_CITY = '${staff.HOME_CITY}',
+      HOME_STATE = '${staff.HOME_STATE}',
+      HOME_ZIP = '${staff.HOME_ZIP}',
+      STAFF_CERTIFICATION = '${staff.STAFF_CERTIFICATION}',
+      STAFF_TRAINING = '${staff.STAFF_TRAINING}',
+      STAFF_ROLE_ID = ${staff.STAFF_ROLE_ID},
+      STAFF_GROUP_ID = ${staff.STAFF_GROUP_ID},
+      STAFF_STATUS_ID = ${staff.STAFF_STATUS_ID},
+      OFFICE_ID = ${staff.OFFICE_ID},
+      EMPLOYMENT_START_DATE = '${staff.EMPLOYMENT_START_DATE}'      
+    WHERE
+      STAFF.STAFF_ID = ${id}
+    `
+  ),
+
+  insertStaffCertification: (certificate) => (
+    `
+    INSERT INTO STAFF_CERTIFICATION (
+      STAFF_ID,
+      CERTIFICATION_ID
+    ) VALUES (
+      ${certificate.STAFF_ID},
+      ${certificate.CERTIFICATION_ID}
+    )
+    `
+  ),
+
+  deleteStaffCertification: (certificate) => (
+    `
+    DELETE FROM STAFF_CERTIFICATION 
+      WHERE 
+      STAFF_ID = ${certificate.STAFF_ID}
+      AND
+      CERTIFICATION_ID = ${certificate.CERTIFICATION_ID}    
+    `
+  ),
+
+  insertStaffExperience: (experience) => (
+    `
+    INSERT INTO STAFF_PROJECT_EXPERIENCE (
+      EXPERIENCE_ID,
+      STAFF_ID,
+      PROJECT_ID
+    ) VALUES (
+      ${experience.EXPERIENCE_ID},
+      ${experience.STAFF_ID},
+      ${experience.PROJECT_ID}
+    )
+    `
+  ),
+
+  deleteStaffExperience: (experience) => (
+    `
+    DELETE FROM STAFF_PROJECT_EXPERIENCE 
+      WHERE 
+      EXPERIENCE_ID = ${experience.EXPERIENCE_ID}
+      AND
+      STAFF_ID = ${experience.STAFF_ID}
+      AND
+      PROJECT_ID = ${experience.PROJECT_ID}    
+    `
+  ),
+
+  customerList: (Condition) => (
+    ` 
+    SELECT
+      CUSTOMER_ID,
+      CUSTOMER_NAME,
+      CUSTOMER_ADDRESS,
+      CUSTOMER_CITY,
+      CUSTOMER_STATE,
+      CUSTOMER_ZIP,
+      CUSTOMER_CONTACT,
+      CONTACT_PHONE      
+    FROM 
+      CUSTOMER          
+    ${Condition} 
+    `   
+  ),
+
+  insertCustomer: (customer) => (
+    `
+    INSERT INTO CUSTOMER (
+      CUSTOMER_NAME,
+      CUSTOMER_ADDRESS,
+      CUSTOMER_CITY,
+      CUSTOMER_STATE,
+      CUSTOMER_ZIP,
+      CUSTOMER_CONTACT,
+      CONTACT_PHONE
+    ) VALUES (
+      '${customer.CUSTOMER_NAME}',
+      '${customer.CUSTOMER_ADDRESS}',
+      '${customer.CUSTOMER_CITY}',
+      '${customer.CUSTOMER_STATE}',
+      '${customer.CUSTOMER_ZIP}',
+      '${customer.CUSTOMER_CONTACT}',
+      '${customer.CONTACT_PHONE}'
+    )
+    `
+  ),
+
+  getCustomerInfo: (id) => (
+    `
+    SELECT
+      *
+    FROM 
+      CUSTOMER 
+    WHERE
+    CUSTOMER.CUSTOMER_ID = ${id}
+    `
+  ),
+
+  updateCustomer: (id, customer) => (
+    `
+    UPDATE CUSTOMER SET 
+      CUSTOMER_NAME = '${customer.CUSTOMER_NAME}',
+      CUSTOMER_ADDRESS = '${customer.CUSTOMER_ADDRESS}',
+      CUSTOMER_CITY = '${customer.CUSTOMER_CITY}',
+      CUSTOMER_STATE = '${customer.CUSTOMER_STATE}',
+      CUSTOMER_ZIP = '${customer.CUSTOMER_ZIP}',
+      CUSTOMER_CONTACT = '${customer.CUSTOMER_CONTACT}',
+      CONTACT_PHONE = '${customer.CONTACT_PHONE}'     
+    WHERE
+    CUSTOMER.CUSTOMER_ID = ${id}
+    `
+  ),
+
+  addCustomerProject: (customerproject) => (
+    `
+    INSERT INTO CUSTOMER_PROJECTS (
+      CUSTOMER_ID,
+      PROJECT_ID
+    ) VALUES (
+      ${customerproject.CUSTOMER_ID},
+      ${customerproject.PROJECT_ID}
+    )
+    `
+  ),
+
+  removeCustomerProject: (customerproject) => (
+    `
+    DELETE FROM CUSTOMER_PROJECTS 
+      WHERE 
+      CUSTOMER_ID = ${customerproject.CUSTOMER_ID}
+      AND
+      PROJECT_ID = ${customerproject.PROJECT_ID}    
+    `
   )
 }
