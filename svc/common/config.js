fec8b8cc311d68cbf7da88f02f156955f545ae6b
minh dong <minh@moblabs.com>
2019-07-18 01:42:50 -0700

    add more information to log

--- svc/common/config.js
+++ svc/common/config.js
@@ -1,4 +1,4 @@
-const masterDB = require('../env.json');
+const environment = require('../env.json');
 
 const encryption = {
   KEY: 'StaffPlanner',
@@ -7,11 +7,13 @@ const encryption = {
 }
 
 module.exports = {
-  DB: { ...masterDB },
+  DB: { ...environment },
   AUTH: { ...encryption },
   DOMAINCHECK: false,
   SENDGRID_API_KEY: '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',
   FROM_EMAIL: 'donotreply@staffplan.io',
   RESET_EXPIRY_IN_HOUR: 24,
-  LOG_LEVEL: 'debug'
+  LOG_LEVEL: 'debug',
+  COMPANY_NAME: environment.companyName,
+  ENVIRONMENT_NAME: environment.name
 }
