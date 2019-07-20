708c93687d196d7315e9d67e969baf353b458b9f
minh dong <minh@moblabs.com>
2019-07-19 22:06:08 -0700

    add healthcheck endpoint

--- svc/modules/route.js
+++ svc/modules/route.js
@@ -1,6 +1,7 @@
 module.exports = (app) => {
   require('./auth/')(app);
   require('./dashboard/')(app);
+  require('./healthcheck/')(app);
   require('./master/')(app);
   require('./project/')(app);
   require('./staff/')(app);
