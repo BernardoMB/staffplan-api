01c9159336c246000761f42e99d8b1d3e56df80a
Muthiah, Ramachandran <>
2019-07-26 14:57:12 +0530

    Fetch Office list

--- svc/modules/master/index.js
+++ svc/modules/master/index.js
@@ -4,7 +4,7 @@ const CONST = require('../../common/const');
 const MODULE = 'common';
 module.exports = (app) => {
   app.get(`/${CONST.API}/master/:modelName`, authenticate.isAuthenticated, master.getMasterList);
-  app.get(`/${CONST.API}/getOfficeNameListing`, authenticate.isAuthenticated, master.getOfficeList);
+  app.get(`/${CONST.API}/${MODULE}/office`, authenticate.isAuthenticated, master.getOfficeList);
   app.get(`/${CONST.API}/${MODULE}/label`, authenticate.isAuthenticated, master.getCustomLabel);
   app.post(`/${CONST.API}/${MODULE}/updatePreference`, authenticate.isAuthenticated, master.updatePreference);
 }
