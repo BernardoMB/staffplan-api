bf21f6a327416fbf7ced4a7b5adc062c69912cbf
Muthiah, Ramachandran <>
2019-08-03 08:19:26 +0530

    support assignment cum open roles edit and delete

--- svc/modules/assignment/assignment.js
+++ svc/modules/assignment/assignment.js
@@ -7,7 +7,7 @@ const getProjectRole = async (req, res) => {
     const connection = await db.connection(req);
     let condition = `PROJECT_TEAM.PROJECT_ID = ${req.params.id} `;
     if (req.query && req.query.active) {
-      condition = `${condition} AND  PROJECT_TEAM.END_DATE >= NOW()`
+      condition = `${condition} AND  PROJECT_TEAM.END_DATE >= CURDATE()`
     }
     const projectTeams = await db.execute(connection, SQL.getProjectTeams(condition));
     util.successResponse(res, projectTeams);
@@ -20,11 +20,11 @@ const getProjectRole = async (req, res) => {
 const insertProjectRole = async (req, res) => {
   try {      
     const roleToCreate = {      
-      ALLOCATION: req.body.project.allocation,      
+      ALLOCATION: req.body.allocation,      
       PROJECT_ID: req.params.id,
-      PROJECT_ROLE_ID: req.body.project.roleId,
-      START_DATE: req.body.project.startDate,
-      END_DATE: req.body.project.endDate
+      PROJECT_ROLE_ID: req.body.roleId,
+      START_DATE: req.body.startDate,
+      END_DATE: req.body.endDate
     };
     const connection = await db.connection(req);
     const rowsAffected = await db.execute(connection, SQL.insertProjectRole(roleToCreate));
@@ -35,26 +35,37 @@ const insertProjectRole = async (req, res) => {
 }
 
 const bulkRoleUpdate = async (req, res) => {
-  try {      
-      const rolesToUpdate = {            
-      PROJECT_ID: req.params.id,
-      START_DATE: req.body.project.startDate,
-      END_DATE: req.body.project.endDate,
-      PLANNED_PROJECT_STAFFIDS: req.body.project.plannedStaffIds
-    };
+  try {
+    const projectId = req.params.id;
+    const startDate = req.body.startDate;
+    const endDate = req.body.endDate;
+    const assignedIds = req.body.roleIds.filter(item => item.staffId);
+    const plannedIds = req.body.roleIds.filter(item => !item.staffId);
     const connection = await db.connection(req);
-    const rowsAffected = await db.execute(connection, SQL.bulkRoleUpdate(rolesToUpdate));
-    util.successResponse(res, rowsAffected);
+    if (assignedIds && assignedIds.length) {
+      await db.execute(connection, SQL.bulkRoleUpdate(
+        'PROJECT_STAFF', startDate, endDate, projectId, assignedIds.map(item => item.id)
+      ));
+    }
+    if (plannedIds && plannedIds.length) {
+      await db.execute(connection, SQL.bulkRoleUpdate(
+        'PLANNED_PROJECT_STAFF', startDate, endDate, projectId, plannedIds.map(item => item.id)
+      ));
+    }
+    util.successResponse(res, true);
   } catch (exception) {
       util.errorResponse(res, exception);
   }
 }
 
 const deleteRole = async (req, res) => {
-  try {    
+  try {
+      const projectId = req.params.id;
+      const id = req.body.roleId;
+      // Based on the assignment the table will be selected
+      const tableName = req.body.staffId ? 'PROJECT_STAFF' : 'PLANNED_PROJECT_STAFF';
       const connection = await db.connection(req);
-      //PLANNED_PROJECT_STAFF id will be the parameter to delete a Role
-      const rowsAffected = await db.execute(connection, SQL.deleteRole(req.params.id));
+      const rowsAffected = await db.execute(connection, SQL.deleteRole(tableName, projectId, id));
       util.successResponse(res, rowsAffected);
     } catch (exception) {
         util.errorResponse(res, exception);
