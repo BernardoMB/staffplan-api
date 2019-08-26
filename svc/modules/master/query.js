c6ec8219d524f55b602e4e8514e2ac17e518b69b
Muthiah, Ramachandran <>
2019-08-26 14:41:32 +0530

    Drop downs - needs to have their values sorted Alphabetically

--- svc/modules/master/query.js
+++ svc/modules/master/query.js
@@ -1,20 +1,22 @@
 module.exports = {
   Master: (condition) => (
-    `SELECT * FROM ${condition}`
+    `SELECT * FROM ${condition} ORDER BY 2`
   ),
   Office: () => (
     `SELECT
       OFFICE_ID,
       OFFICE_NAME
     FROM 
-      OFFICE`
+      OFFICE
+    ORDER BY OFFICE_NAME`
   ),
   getYears: () => (
     `SELECT
       YEAR ID, YEAR
     FROM
       CALENDAR 
-    GROUP BY YEAR`
+    GROUP BY YEAR
+    ORDER BY YEAR`
   ),
   Label: () => (
     `SELECT MODULE_NAME, FIELD_NAME, FIELD_VALUE FROM CUSTOM_LABEL ORDER BY MODULE_NAME, FIELD_NAME`
