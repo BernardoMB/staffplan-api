167509649873b748a326e3858cf0f73b3233e28f
Anna Rathna T <annarathna.t@gmail.com>
2019-08-29 22:03:02 +0530

    Admin service for create and update user

--- svc/modules/admin/admin.js
+++ svc/modules/admin/admin.js
@@ -41,6 +41,53 @@ const insertCalendar = async (req, res) => {
   }
 }
 
-module.exports = {
-  insertCalendar
+const insertUser = async (req, res) => {
+  try {
+    const userDefault = {
+      ROLE_ID: null,
+      FIRST_NAME: '',
+      MIDDLE_INITIAL: '',
+      LAST_NAME: '',
+      EMAIL_ID: '',
+      PASSWORD: '',
+      VERIFIED: 0,
+      ADDRESS: '',
+      CITY: '',
+      COUNTRY: '',
+      ZIP: '',
+      ISACTIVE: 0
+    };
+    const userInfo = req.body;
+    const userToCreate = Object.assign(userDefault, userInfo);
+    const connection = await db.connection(req);
+    const rowsAffected = await db.execute(connection, SQL.insertUser(userToCreate));
+    util.successResponse(res, rowsAffected);
+  } catch (exception) {
+      util.errorResponse(res, exception);
+  }
 }
+
+const updateUser = async (req, res) => {
+  try {
+    const user = req.body;    
+    const connection = await db.connection(req);
+    const result = await db.execute(connection, SQL.getUserInfoByID(req.params.id));
+    let detailsToUpdate = {};
+    if (result && result.length > 0) {
+      detailsToUpdate = result[0];
+    }
+    const userToUpdate = Object.assign(detailsToUpdate, user);
+    const rowsAffected = await db.execute(connection, SQL.updateUser(req.params.id, userToUpdate));
+    util.successResponse(res, rowsAffected);
+    } catch (exception) {
+        util.errorResponse(res, exception);
+    }
+}
+
+module.exports = {
+  insertCalendar,
+  insertUser,
+  updateUser
+}
+
+
