2616b535c64894d96abdd2690a12befdab9f855e
Anna Rathna T <annarathna.t@gmail.com>
2019-08-29 22:03:02 +0530

    Admin service for create and update user

--- svc/modules/admin/index.js
+++ svc/modules/admin/index.js
@@ -5,4 +5,6 @@ const CONST = require('../../common/const');
 const MODULE = 'admin';
 module.exports = (app) => {
   app.put(`/${CONST.API}/${MODULE}/calendar/:year`, authenticate.isAuthenticated, admin.insertCalendar);
+  app.put(`/${CONST.API}/${MODULE}/user`, authenticate.isAuthenticated, admin.insertUser);
+  app.post(`/${CONST.API}/${MODULE}/:id/info`, authenticate.isAuthenticated, admin.updateUser);
 }
--- svc/modules/admin/query.js
+++ svc/modules/admin/query.js
@@ -3,5 +3,66 @@ module.exports = {
     `INSERT INTO CALENDAR (YEAR, WEEK, START_DATE, END_DATE) VALUES
       ${values}
     `
+  ),
+  insertUser: (user) => (
+    `
+    INSERT INTO USERS (
+      ROLE_ID,
+      FIRST_NAME,
+      MIDDLE_INITIAL,
+      LAST_NAME,
+      EMAIL_ID,
+      PASSWORD,
+      VERIFIED,
+      ADDRESS,
+      CITY,
+      COUNTRY,
+      ZIP,
+      ISACTIVE
+    ) VALUES (
+      ${user.ROLE_ID},
+      '${user.FIRST_NAME}',
+      '${user.MIDDLE_INITIAL}',
+      '${user.LAST_NAME}',
+      '${user.EMAIL_ID}',
+      '${user.PASSWORD}',
+      ${user.VERIFIED},
+      '${user.ADDRESS}',
+      '${user.CITY}',
+      '${user.COUNTRY}',
+      '${user.ZIP}',
+      ${user.ISACTIVE}
+    )  
+    `
+  ),
+  getUserInfoByID: (id) => (
+    `
+    SELECT
+      *
+    FROM 
+      USERS 
+    WHERE
+      USERS.USER_ID = ${id}
+    `
+  ),
+  updateUser: (id, user) => (
+    `
+    UPDATE USERS SET
+      ROLE_ID = ${user.ROLE_ID},
+      FIRST_NAME = '${user.FIRST_NAME}',
+      MIDDLE_INITIAL = '${user.MIDDLE_INITIAL}',
+      LAST_NAME = '${user.LAST_NAME}',
+      EMAIL_ID = '${user.EMAIL_ID}',
+      PASSWORD = '${user.PASSWORD}',
+      VERIFIED = ${user.VERIFIED},
+      ADDRESS = '${user.ADDRESS}',
+      CITY = '${user.CITY}',
+      COUNTRY = '${user.COUNTRY}',
+      ZIP = '${user.ZIP}',
+      ISACTIVE = ${user.ISACTIVE}
+    WHERE
+      USERS.USER_ID = ${id}
+    `
   )
 };
+
