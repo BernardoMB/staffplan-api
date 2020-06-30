a76864656cd62bcf6bc8b3b55288ef7726530b7b
enrifransch <enrifransch>
2020-06-30 13:32:05 -0500

    Fix file

---
+++ svc/common/config.js
@@ -0,0 +1,20 @@
+const environment = require('../env.json');
+
+const encryption = {
+  KEY: 'StaffPlanner',
+  SUPERSECRETTIME: 6060,
+  SUPERSECRETREFRESHTIME: 6000
+}
+
+module.exports = {
+  DB: { ...environment },
+  AUTH: { ...encryption },
+  DOMAINCHECK: environment.domainCheck,
+  SENDGRID_API_KEY: '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',
+  FROM_EMAIL: 'donotreply@staffplan.io',
+  RESET_EXPIRY_IN_HOUR: 24,
+  LOG_LEVEL: 'debug',
+  COMPANY_NAME: environment.companyName,
+  ENVIRONMENT_NAME: environment.name,
+  AWS: { ...environment.aws }
+}
