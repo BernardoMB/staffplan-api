ca9b8e7337b5840b992002f61f49d2d1fe648714
enrifransch <enrifransch>
2020-08-20 20:11:23 -0500

    Add fi;e

---
+++ svc/test/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    "host": "rds.coqokg7f9b0o.us-west-1.rds.amazonaws.com",
+    "user": "staff_root",
+    "password": "6HTpVVZeC",
+    "port": 3306,
+    "database": "dev_internal",
+    "name": "acme-demo",
+}
