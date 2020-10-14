46d3c06f2fff3d381b636bd03805fba8a2d0e27c
BernardoMB <bmondragonbrozon@gmail.com>
2020-10-14 18:24:41 -0500

    Added delete office endpoint

--- svc/modules/office/index.js
+++ svc/modules/office/index.js
@@ -4,6 +4,11 @@ const CONST = require('../../common/const');
 const MODULE = 'office';
 
 module.exports = (app) => {
+    app.put(
+        `/${CONST.API}/${MODULE}/details`,
+        authenticate.isAuthenticated,
+        office.insertOfficeDetail
+    );
     app.post(
         `/${CONST.API}/${MODULE}/list`,
         authenticate.isAuthenticated,
@@ -19,14 +24,14 @@ module.exports = (app) => {
         authenticate.isAuthenticated,
         office.getOfficeDetailById
     );
-    app.put(
-        `/${CONST.API}/${MODULE}/details`,
-        authenticate.isAuthenticated,
-        office.insertOfficeDetail
-    );
     app.post(
         `/${CONST.API}/${MODULE}/:id/details`,
         authenticate.isAuthenticated,
         office.updateOfficeDetail
+    );
+    app.delete(
+        `/${CONST.API}/${MODULE}/:id`,
+        authenticate.isAuthenticated,
+        office.deleteOfficeById
     );
 };
